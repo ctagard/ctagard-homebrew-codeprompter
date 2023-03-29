8a2bf18e80722b6f5ae175d5b8bf8d1fafbf3516
ctagard <ctagard19@gmail.com>
2023-03-28 22:08:30 -0400

    ready for release to homebrew

---
+++ homebrew-codeprompter/formula/codeprompter.rb
@@ -0,0 +1,18 @@
+class Codeprompter < Formula
+  desc "Generates a prompt file for Rust, Go, and JavaScript projects"
+  homepage "https://github.com/ctagard/CodePrompter"
+  url "https://github.com/ctagard/CodePrompter/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "b72de62cda7f83eb26a97fcf3c5a5888356f895195fe820f40816a2abb438ad7"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "--release", "--locked"
+    bin.install "target/release/codeprompter"
+  end
+
+  test do
+    system "#{bin}/codeprompter", "--version"
+  end
+end
